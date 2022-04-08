bcfa30af41a11c294652109e59a15a84ddb1be4a
YusufAlperUnal <yalperunal@gmail.com>
2022-04-08 13:52:46 +0300

    igniteui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService} from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'alper-angular-course1';
+  constructor(private toastr: ToastrService, 
+    private spinner: NgxSpinnerService){}
+  ngOnInit(): void {
+    this.showSuccess()
+    this.showSpinner()
+  }
+  showSuccess(){
+    this.toastr.success("Angular Projeme Hoşgeldiniz",'Giriş Başarılı !');
+  } 
+
+  showSpinner(){
+    this.spinner.show();
+    setTimeout(() => {
+    this.spinner.hide()
+  }, 5000);
+  }
 }
